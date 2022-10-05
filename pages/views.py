fea35084a903ccc4b4f008f0caa6c5a96a958feb
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2022-10-05 11:09:56 +0500

    Some changes have been made

--- pages/views.py
+++ pages/views.py
@@ -3,10 +3,10 @@ from . import models
 
 # Create your views here.
 def HomePagesView(request):
-    offer_works = models.OfferWorks.objects.all()
-    happy_clients = models.HappyClients.objects.all()
-    agents = models.Agents.objects.all()
-    blogs = models.ResentBlog.objects.all()
+    offer_works = models.OfferWorks.objects.all().order_by('-date')
+    happy_clients = models.HappyClients.objects.all().order_by('-client_date')
+    agents = models.Agents.objects.all().order_by('-date')
+    blogs = models.ResentBlog.objects.all().order_by('-date')
 
     context = {'offer_works':offer_works,
               'happy_clients': happy_clients,
@@ -16,11 +16,11 @@ def HomePagesView(request):
     return render(request, 'index.html', context)
 
 def AgentPagesView(request):
-    agents = models.Agents.objects.all()
+    agents = models.Agents.objects.all().order_by('-date')
     return render(request, 'agent.html', {'agents': agents})
 
 def PropertiesPagesView(request):
-    offer_works = models.OfferWorks.objects.all()
+    offer_works = models.OfferWorks.objects.all().order_by('-date')
     return render(request, 'properties.html', {'offer_works': offer_works})
 
 def ContactPagesView(request):
